fd721831f7bdfee3a9dbbda913b63dd29d682fe7
Himanshu Gola <angularcli19@gmail.com>
2024-12-28 13:20:56 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { ServiceComponent } from './service/service.component';
-import { ErrorComponent } from './error/error.component';
-import { MainComponent } from './main/main.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ServiceComponent,ErrorComponent,MainComponent,RouterLink],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,6 @@
 import { Routes } from '@angular/router';
-import { ServiceComponent } from './service/service.component';
-import { ErrorComponent } from './error/error.component';
-import { MainComponent } from './main/main.component';
-import { KrishnaComponent } from './krishna/krishna.component';
-import { RadheComponent } from './radhe/radhe.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:ServiceComponent
-    },
-    
-    {
-        path:'main',
-        component:MainComponent
-    },
-    {
-        path:'radhe',
-        component:RadheComponent
-    },
-    {
-        path:'krishna',
-        component:KrishnaComponent
-    },
-    {
-        path:'**',
-        component:ErrorComponent
-    }
+   
 
 ];
